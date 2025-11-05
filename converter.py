f33d36102e64a5d105c80e98eecbade249c68f36
srgsuh <srg.suhoverhov@gmail.com>
2025-11-05 09:24:53 +0200

    SImplify convertX method

--- converter.py
+++ converter.py
@@ -5,14 +5,9 @@ import pandas as pd
 def enumerator(values: Iterable[str]) -> dict[str, int]:
     return {x:idx for idx, x in enumerate(values)}
 
-def columnsMapper(columnsStr: list[str], df: pd.DataFrame)->dict[str, dict[str, int]]:
+def columnsMapper(columnsStr: list[str], df: pd.DataFrame) -> dict[str, dict[str, int]]:
     return {col_name: enumerator(df[col_name].unique()) for col_name in columnsStr}
 
-def convertX(df: pd.DataFrame, mapper: dict[str, dict[str, int]])-> pd.DataFrame:
-    dct = {}
-    for col_name in df.columns:
-        if col_name in mapper:
-            dct[col_name] = [mapper[col_name][value] for value in df[col_name]]
-        else:
-            dct[col_name] = df[col_name]
+def convertX(df: pd.DataFrame, mapper: dict[str, dict[str, int]]) -> pd.DataFrame:
+    dct = {col: df[col] if col not in mapper else [mapper[col][val] for val in df[col]] for col in df.columns}
     return pd.DataFrame(dct)

8c462b70d75e78c8d52157cb19480c14ae496c53
srgsuh <srg.suhoverhov@gmail.com>
2025-11-09 12:40:23 +0200

    Implement game of life generator methods

--- game_of_life.py
+++ game_of_life.py
@@ -4,16 +4,15 @@ from numpy.typing import NDArray
 
 _gen = np.random.default_rng()
 
-def random_matrix(size: int, alive_val: np.uint8 | int, dead_val: np.uint8 | int) -> NDArray[np.uint8] :
+def _random_gol(size: int, alive_val: np.uint8 | int, dead_val: np.uint8 | int) -> NDArray[np.uint8]:
     rm: NDArray[np.uint8] = np.empty((size, size), dtype=np.uint8)
     for r in range(size):
         for c in range(size):
             rm[r, c] = dead_val if _gen.random() < 0.5 else alive_val
     return rm
 
-def game_of_life(size: int, alive_val: np.uint8, dead_val: np.uint8)->Generator[NDArray[np.uint8], None, None]:
-    prev: NDArray[np.uint8] = random_matrix(size, alive_val, dead_val)
-    yield prev
+def game_of_life(size: int, alive_val: np.uint8 | int = 220, dead_val: np.uint8 | int = 0, init_data: NDArray[np.uint8] | None = None)->Generator[NDArray[np.uint8], None, None]:
+    prev: NDArray[np.uint8] = _random_gol(size, alive_val, dead_val) if init_data is None else init_data
 
     def alive_neighbors(row: int, col: int) -> int:
         count_alive: int = -1 if prev[row, col] == alive_val else 0
@@ -23,20 +22,24 @@ def game_of_life(size: int, alive_val: np.uint8, dead_val: np.uint8)->Generator[
         return count_alive
 
 
-    def next_gen() -> NDArray[np.uint8]:
+    def next_generation() -> NDArray[np.uint8]:
         mx: NDArray[np.uint8] = np.empty((size, size), dtype=np.uint8)
         for row in range(size):
             for col in range(size):
                 count_alive: int = alive_neighbors(row, col)
                 mx[row, col] = alive_val if (count_alive == 3 or 
-                        (count_alive == 2 and mx[row, col] == alive_val)) else dead_val
+                        (count_alive == 2 and prev[row, col] == alive_val)) else dead_val
         return mx
 
     while True:
-        current: NDArray[np.uint8] = next_gen()
+        current: NDArray[np.uint8] = next_generation()
+        yield prev
+        if np.array_equal(prev, current):
+            break
         prev = current
-        yield current
 
 if __name__ == "__main__":
-    m = random_matrix(10, alive_val=1, dead_val=0)
-    print(m)
+    for step, mx in enumerate(game_of_life(3, 1, 0, np.array([[1,1,0],[0,1,1],[0,0,0]], dtype=np.uint8))):
+        print(mx)
+        if step == 10:
+            break

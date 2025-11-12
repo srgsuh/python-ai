e9b1687935c0d84fe58f9a6bfaf84ac642ddd3df
srgsuh <srg.suhoverhov@gmail.com>
2025-11-12 17:34:41 +0200

    Check for blank strings in the vocabulary

--- text_sklearn_model.py
+++ text_sklearn_model.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
+from numpy.typing import NDArray
 
 BASE_TOL: float = 1e-6
 
@@ -15,8 +16,11 @@ class TextModel:
 
     def __build_model(self, text: str) -> None:
         self.__vectorizer = TfidfVectorizer()
-        self.__sentences = [s.strip() for s in text.split('.')]
+        self.__sentences = [s for s in text.split('.')]
         self.__model = self.__vectorizer.fit_transform(self.__sentences)
+    
+    def get_feature_names_out(self) -> NDArray:
+        return self.__vectorizer.get_feature_names_out()
 
     def getAnswers(self, question: str, nAnswers: int = 1) -> list[str]:
         question_vector = self.__vectorizer.transform([question])
@@ -31,5 +35,6 @@ class TextModel:
 if __name__ == "__main__":
     text = read_file("tortoise_and_a_hare.txt")
     model = TextModel(text)
+    print(model.get_feature_names_out())
     for x in model.getAnswers("Why did the hare agree to the proposal", 3):
         print(x)

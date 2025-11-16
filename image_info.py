865259f89e951f8fc30b5912152d5a0e286ffa90
srgsuh <srg.suhoverhov@gmail.com>
2025-11-16 11:24:11 +0200

    Implementation with the big dataframe

--- image_info.py
+++ image_info.py
@@ -1,5 +1,6 @@
 from ultralytics.models import YOLO
 import numpy as np
+import pandas as pd
 from numpy.typing import NDArray
 from ultralytics.engine.results import Boxes, BaseTensor
 
@@ -15,9 +16,21 @@ class ImageInfo:
         results: list[BaseTensor] = self.__model(img_path)
         self.boxes: BaseTensor  = results[0].boxes.cpu()
         self.ids = results[0].boxes.cls.cpu().numpy()
+        self.xywh = results[0].boxes.xywh.cpu().numpy()
+        self.xyxy = results[0].boxes.xyxy.cpu().numpy()
+        self.conf = results[0].boxes.conf.cpu().numpy()
         self.class_by_id: dict[int, str] = results[0].names
         self.id_by_class: dict[str, int] = {value: key for key, value in self.class_by_id.items()}
+        self.__build_data_frame()
     
+    def __build_data_frame(self) -> None:
+        df_xywh: pd.DataFrame = pd.DataFrame(self.xywh, columns=["x_center", "y_center", "width", "height"])
+        df_xyxy: pd.DataFrame = pd.DataFrame(self.xyxy, columns=["xmin", "ymin", "xmax", "ymax"])
+        self.df: pd.DataFrame = pd.concat([df_xywh, df_xyxy], axis=1)
+        self.df["class"] = [self.class_by_id[class_id] for class_id in self.ids]
+        self.df["confidence"] = self.conf
+        print(self.df)
+
     def boxesClass(self, class_name: str) -> list[int]:
         class_id = self.id_by_class[class_name]
         indices = np.nonzero(self.ids == class_id)[0]
@@ -26,6 +39,9 @@ class ImageInfo:
     def boxInfo(self, box_index):
         raise NotImplementedError()
     
+    def dataFrame(self) -> pd.DataFrame:
+        return self.df
+
 if __name__ == "__main__":
     ii: ImageInfo = ImageInfo("./bus.jpg")
     print(ii.boxesClass("person"))

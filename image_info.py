200d8da3e20dc402d84058b1441b352ed5965de7
srgsuh <srg.suhoverhov@gmail.com>
2025-11-16 10:53:40 +0200

    Implement boxesClass method

--- image_info.py
+++ image_info.py
@@ -1,17 +1,31 @@
 from ultralytics.models import YOLO
+import numpy as np
+from numpy.typing import NDArray
+from ultralytics.engine.results import Boxes, BaseTensor
 
-DEFAULT_MODEL = "yolov8m-seg.pt"
-MODELS = ["yolov8m-seg.pt"]
-
-CLASSES: dict[str, str] = {
-    "0": "person",
+BAGGAGE_CLASSES: dict[str, str] = {
     "24": "backpack",
     "26": "handbag",
     "28": "suitcase"
 }
 
 class ImageInfo:
-    def __init__(self, img_path: str, model_name: str = DEFAULT_MODEL):
-        if model_name not in [MODELS]:
-            raise ValueError(f"Unacceptable model name: {model_name}")
-        self.__model = YOLO(model_name)
+    def __init__(self, img_path: str):
+        self.__model = YOLO("yolov8m-seg.pt")
+        results: list[BaseTensor] = self.__model(img_path)
+        self.boxes: BaseTensor  = results[0].boxes.cpu()
+        self.ids = results[0].boxes.cls.cpu().numpy()
+        self.class_by_id: dict[int, str] = results[0].names
+        self.id_by_class: dict[str, int] = {value: key for key, value in self.class_by_id.items()}
+    
+    def boxesClass(self, class_name: str) -> list[int]:
+        class_id = self.id_by_class[class_name]
+        indices = np.nonzero(self.ids == class_id)[0]
+        return indices.tolist()
+    
+    def boxInfo(self, box_index):
+        raise NotImplementedError()
+    
+if __name__ == "__main__":
+    ii: ImageInfo = ImageInfo("./bus.jpg")
+    print(ii.boxesClass("person"))

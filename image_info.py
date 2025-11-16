2874d8764d275a35b408f061380b9785099a71b5
srgsuh <srg.suhoverhov@gmail.com>
2025-11-16 13:10:19 +0200

    Refactor constructor

--- image_info.py
+++ image_info.py
@@ -9,24 +9,19 @@ BAGGAGE_CLASSES: list[str] = ["backpack","handbag","suitcase"]
 
 class ImageInfo:
     def __init__(self, img_path: str):
-        self.__model = YOLO("yolov8m-seg.pt")
-        results: list[BaseTensor] = self.__model(img_path)
-        self.boxes: BaseTensor  = results[0].boxes.cpu()
-        self.ids = results[0].boxes.cls.cpu().numpy()
-        self.xywhn = results[0].boxes.xywhn.cpu().numpy()
-        self.xyxy = results[0].boxes.xyxy.cpu().numpy()
-        self.conf = results[0].boxes.conf.cpu().numpy()
-        self.class_by_id: dict[int, str] = results[0].names
-        self.id_by_class: dict[str, int] = {value: key for key, value in self.class_by_id.items()}
-        self.__build_data_frame()
-    
-    def __build_data_frame(self) -> None:
-        df_xywh: pd.DataFrame = pd.DataFrame(self.xywhn, columns=["x_center", "y_center", "width", "height"])
-        df_xyxy: pd.DataFrame = pd.DataFrame(self.xyxy, columns=["xmin", "ymin", "xmax", "ymax"])
+        model = YOLO("yolov8m-seg.pt")
+        results: list[BaseTensor] = model(img_path)
+        
+        xywhn = results[0].boxes.xywhn.cpu().numpy()
+        xyxy = results[0].boxes.xyxy.cpu().numpy()
+        ids = results[0].boxes.cls.cpu().numpy()
+        conf = results[0].boxes.conf.cpu().numpy()
+
+        df_xywh: pd.DataFrame = pd.DataFrame(xywhn, columns=["x_center", "y_center", "width", "height"])
+        df_xyxy: pd.DataFrame = pd.DataFrame(xyxy, columns=["xmin", "ymin", "xmax", "ymax"])
         self.df: pd.DataFrame = pd.concat([df_xywh, df_xyxy], axis=1)
-        self.df["class_name"] = [self.class_by_id[class_id] for class_id in self.ids]
-        self.df["confidence"] = self.conf
-        print(self.df)
+        self.df["class_name"] = [results[0].names[class_id] for class_id in ids]
+        self.df["confidence"] = conf
 
     def boxesClass(self, class_name: str) -> list[int]:
         return self.df.index[self.df.class_name == class_name].to_list()

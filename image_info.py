6f3dc4682f2b93f7951e82eddd517f8f04836484
srgsuh <srg.suhoverhov@gmail.com>
2025-11-16 18:00:10 +0200

    Add empty DataFrame building in case of unrecognized picture

--- image_info.py
+++ image_info.py
@@ -2,28 +2,46 @@ from ultralytics.models import YOLO
 import numpy as np
 import pandas as pd
 from numpy.typing import NDArray
-from ultralytics.engine.results import Boxes, BaseTensor
+from ultralytics.engine.results import Boxes, BaseTensor, Results
+from torch import Tensor
 import math
 
 BAGGAGE_CLASSES: list[str] = ["backpack","handbag","suitcase"]
+COLUMNS: list[str] = ["x_center","y_center","width", "height","xmin","ymin","xmax","ymax","class_name","confidence"]
+
+def get_ndarray(array: Tensor | NDArray) -> NDArray:
+    if isinstance(array, Tensor):
+        return array.cpu().numpy()
+    return array
+
 
 class ImageInfo:
     def __init__(self, img_path: str):
         """Create the ImageInfo instance from a path to an image file. Use the yolov8m-seg model internally"""
+        self.__build_df(img_path)
+
+    def __build_df(self, img_path: str) -> None:
         model: YOLO = YOLO("yolov8m-seg.pt")
-        results: list[BaseTensor] = model(img_path)
-        
-        xywhn: NDArray = results[0].boxes.xywhn.cpu().numpy()
-        xyxy: NDArray = results[0].boxes.xyxy.cpu().numpy()
-        ids: NDArray = results[0].boxes.cls.cpu().numpy()
-        conf: NDArray = results[0].boxes.conf.cpu().numpy()
+        result: list[Results] = model(img_path)
+        if result and result[0].boxes:
+            boxes: Boxes = result[0].boxes
+            xywhn: NDArray = get_ndarray(boxes.xywhn)
+            xyxy: NDArray = get_ndarray(boxes.xyxy)
+            class_ids: NDArray = get_ndarray(boxes.cls)
+            conf: NDArray = get_ndarray(boxes.conf)
+            names: dict[int, str] = result[0].names
 
-        df_xywh: pd.DataFrame = pd.DataFrame(xywhn, columns=["x_center", "y_center", "width", "height"])
-        df_xyxy: pd.DataFrame = pd.DataFrame(xyxy, columns=["xmin", "ymin", "xmax", "ymax"])
-        self.df: pd.DataFrame = pd.concat([df_xywh, df_xyxy], axis=1)
-        self.df["class_name"] = [results[0].names[class_id] for class_id in ids]
-        self.df["confidence"] = conf
+            df_xywh: pd.DataFrame = pd.DataFrame(xywhn, columns=["x_center", "y_center", "width", "height"])
+            df_xyxy: pd.DataFrame = pd.DataFrame(xyxy, columns=["xmin", "ymin", "xmax", "ymax"])
+            self.df: pd.DataFrame = pd.concat([df_xywh, df_xyxy], axis=1)
+            self.df["class_name"] = [names[class_id] for class_id in class_ids]
+            self.df["confidence"] = conf
+        else:
+            self.df = pd.DataFrame(columns=COLUMNS)
 
+    def __len__(self) -> int:
+        return len(self.df)
+    
     def __indices_by_class_list(self, classes: list[str]) -> list[int]:
         return self.df.index[self.df.class_name.isin(classes)].to_list()
 
@@ -73,8 +91,17 @@ class ImageInfo:
 
 if __name__ == "__main__":
     ii: ImageInfo = ImageInfo("./bag.jpg")
+    print(f"LENGTH: {len(ii)}")
     print(ii.boxesClass("person"))
+    print(f"DataFrame: {ii.dataFrame()}")
     print("BoxInfo: ", ii.boxInfo(0))
     print("COORD: ", ii.relative_center(0), ii.relative_center(1))
     print("DISTANCE: ", ii.distance(0, 1))
     print(f"Dict: {ii.suitcaseHandbagPerson(1.0)}")
+
+    empty: ImageInfo = ImageInfo("./empty.jpg")
+    print(f"LENGTH: {len(empty)}")
+    print(f"EMPTY DataFrame: {empty.dataFrame()}")
+    print(f"EMPTY Dict: {empty.suitcaseHandbagPerson(1.0)}")
+    print(f"EMPTY persons: {empty.boxesClass("person")}")
+    print(f"EMPTY bags: {empty.baggage_indices()}")

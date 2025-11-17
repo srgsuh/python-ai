9a5baa46070ee5310230bde199b360dfad32652c
srgsuh <srg.suhoverhov@gmail.com>
2025-11-17 14:51:39 +0200

    Refactor create_folders

--- create_folders.py
+++ create_folders.py
@@ -7,13 +7,17 @@ def join_path(parent_dir_path: str, inner_obj_name: str) -> str:
     return parent_dir_path + os.sep + inner_obj_name
 
 ROOT_DIR = 'datasets'
+
+IMAGE_SUB = 'images'
+LABEL_SUB = 'labels'
+
 TRAIN_DIR = join_path(ROOT_DIR, 'train')
-TRAIN_IMAGES = join_path(TRAIN_DIR, 'images')
-TRAIN_LABELS = join_path(TRAIN_DIR , 'labels')
+TRAIN_IMAGES = join_path(TRAIN_DIR, IMAGE_SUB)
+TRAIN_LABELS = join_path(TRAIN_DIR , LABEL_SUB)
 
 VAL_DIR = join_path(ROOT_DIR, 'val')
-VAL_IMAGES = join_path(VAL_DIR, 'images')
-VAL_LABELS = join_path(VAL_DIR , 'labels')
+VAL_IMAGES = join_path(VAL_DIR, IMAGE_SUB)
+VAL_LABELS = join_path(VAL_DIR , LABEL_SUB)
 
 for folder in [TRAIN_IMAGES, TRAIN_LABELS, VAL_IMAGES, VAL_LABELS]:
     make_dir(folder)

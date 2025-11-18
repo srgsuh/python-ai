2313e6b48a78c7d1706f03ecfc78a79c234a69d0
srgsuh <srg.suhoverhov@gmail.com>
2025-11-18 13:07:38 +0200

    refactor to reflect the changes in Shape interface

--- create_training_data.py
+++ create_training_data.py
@@ -8,17 +8,17 @@ WIDTH: int = 256
 HEIGHT: int = 256
 IMAGE_EXT = '.jpg'
 LABEL_EXT = '.txt'
-N_TESTS = 8
+N_TESTS = 20
 
-def create_training_case(parent_folder: str, f_name: str, shape: ShapeImage) -> None:
-    write_text(join(parent_folder, LABEL_SUB, f_name+LABEL_EXT), shape.label())
+def create_training_case(parent_folder: str, f_name: str, shape: ShapeImage, obj_index: int = 0) -> None:
+    write_text(join(parent_folder, LABEL_SUB, f_name+LABEL_EXT), shape.label(obj_index))
     write_image(join(parent_folder, IMAGE_SUB, f_name+IMAGE_EXT), shape.image())
 
 
 def generate_test_circles(parent_folder: str, n_tests: int) -> None:
     for idx in range(1, n_tests + 1):
         shape: ShapeImage = random_circle(WIDTH, HEIGHT)
-        create_training_case(parent_folder, 'circle_'+str(idx).zfill(3), shape)
+        create_training_case(parent_folder, 'circle_'+str(idx).zfill(3), shape, 0)
 
 if __name__ == '__main__':
     empty_folders(TRAIN_DIR, VAL_DIR)

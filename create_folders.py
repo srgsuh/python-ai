41b1301fccfde3cd264306279d00900a291b5fb7
srgsuh <srg.suhoverhov@gmail.com>
2025-11-18 12:37:44 +0200

    Move all helper routines to a separate module utils

--- create_folders.py
+++ create_folders.py
@@ -1,11 +1,6 @@
 import os
 from os.path import join
 
-def clear_all_files(root: str):
-    for dirpath, _, filenames in os.walk(root):
-        for filename in filenames:
-            os.remove(join(dirpath, filename))
-
 ROOT_DIR = 'datasets'
 
 IMAGE_SUB = 'images'
@@ -19,5 +14,6 @@ VAL_DIR = join(ROOT_DIR, 'val')
 VAL_IMAGES = join(VAL_DIR, IMAGE_SUB)
 VAL_LABELS = join(VAL_DIR , LABEL_SUB)
 
-for folder in [TRAIN_IMAGES, TRAIN_LABELS, VAL_IMAGES, VAL_LABELS]:
-    os.makedirs(folder, exist_ok=True)
+if __name__ == "__main__":
+    for folder in [TRAIN_IMAGES, TRAIN_LABELS, VAL_IMAGES, VAL_LABELS]:
+        os.makedirs(folder, exist_ok=True)
--- create_training_data.py
+++ create_training_data.py
@@ -1,9 +1,8 @@
-import cv2
-import numpy as np
 from os.path import join
-from create_folders import TRAIN_DIR,VAL_DIR,IMAGE_SUB,LABEL_SUB, clear_all_files
+from create_folders import TRAIN_DIR,VAL_DIR,IMAGE_SUB,LABEL_SUB
 from circle_image import random_circle
 from shape_image import ShapeImage
+from utils import write_image, write_text, empty_folders
 
 WIDTH: int = 256
 HEIGHT: int = 256
@@ -11,26 +10,18 @@ IMAGE_EXT = '.jpg'
 LABEL_EXT = '.txt'
 N_TESTS = 8
 
-def save_text(file_path: str, text: str) -> None:
-    with open(file_path, 'w') as f:
-        f.write(text)
-
-def save_image(file_path: str, img: np.ndarray) -> None:
-    cv2.imwrite(file_path, img)
-
-def save_training_case(parent_folder: str, f_name: str, shape: ShapeImage) -> None:
-    save_text(join(parent_folder, LABEL_SUB, f_name+LABEL_EXT), shape.label())
-    save_image(join(parent_folder, IMAGE_SUB, f_name+IMAGE_EXT), shape.image())
+def create_training_case(parent_folder: str, f_name: str, shape: ShapeImage) -> None:
+    write_text(join(parent_folder, LABEL_SUB, f_name+LABEL_EXT), shape.label())
+    write_image(join(parent_folder, IMAGE_SUB, f_name+IMAGE_EXT), shape.image())
 
 
 def generate_test_circles(parent_folder: str, n_tests: int) -> None:
     for idx in range(1, n_tests + 1):
         shape: ShapeImage = random_circle(WIDTH, HEIGHT)
-        save_training_case(parent_folder, 'circle_'+str(idx).zfill(3), shape)
+        create_training_case(parent_folder, 'circle_'+str(idx).zfill(3), shape)
 
 if __name__ == '__main__':
-    clear_all_files(TRAIN_DIR)
-    clear_all_files(VAL_DIR)
+    empty_folders(TRAIN_DIR, VAL_DIR)
     generate_test_circles(TRAIN_DIR, N_TESTS)
     generate_test_circles(VAL_DIR, max(1, N_TESTS // 10))
         

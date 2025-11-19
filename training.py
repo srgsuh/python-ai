a0e64331e87d3897f6651bfec620ffc3722a9627
srgsuh <srg.suhoverhov@gmail.com>
2025-11-19 19:59:52 +0200

    Technical commit

--- training.py
+++
@@ -1,5 +0,0 @@
-from ultralytics.models import YOLO
-
-if __name__ == '__main__':
-    model = YOLO("yolov8m.pt")
-    model.train(data="./datasets/data.yaml", batch=2, epochs=30, imgsz=256, name="circle_rect")

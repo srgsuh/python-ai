e37ac2466990dd030ce7a339d6bc2b72bd930809
srgsuh <srg.suhoverhov@gmail.com>
2025-11-20 13:34:48 +0200

    Implement the mobile phone pattern

--- regular_expressions.py
+++ regular_expressions.py
@@ -5,8 +5,22 @@ def pwd_pattern() -> str:
     return r"(?=.*[A-Z])(?=.*[a-z])(?=.*[#$%])(?=.*[\d])[A-Za-z_#$%\d]{8,}"
 
 def ipv4_pattern() -> str:
+    """returns regexp as match pattern of IPv4 address
+       comprises of 4 octets separated by dot
+       each octet contains 1-3 symbols from 0 to 255
+    """
     return  r"(([0-1]?\d{1,2}|25[0-5]|2[0-4][0-9])\.){3}([0-1]?\d{1,2}|25[0-5]|2[0-4][0-9])"
 
 def israel_mobile_pattern() -> str:
-    #TODO
-    raise NotImplementedError()
+    """returns regexp for mobile phone Israel number
+       +972- - Israel preffix (not mandatary)
+       Operator preffix 0 (only without +972-)
+       50,51, 52, 53, 54, 55, 56, 57,58, 59
+       optional dash
+       7 digits as follows
+       xxxxxxx
+       xxx-xx-xx
+       x-xx-xx-xx
+    """
+    
+    return r"(\+972-|0)5\d-?(\d{7}|(\d-?\d{2}-\d{2}-\d{2}))"

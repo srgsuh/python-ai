7a2c4ceb423de3e6f03db7c53b77cd97a659bb96
srgsuh <srg.suhoverhov@gmail.com>
2025-11-25 19:15:01 +0200

    Add iterable_items_pattern function

--- regular_expressions.py
+++ regular_expressions.py
@@ -1,3 +1,6 @@
+from typing import Iterable
+import re
+
 def unsigned_float_number_pattern() -> str:
     """Define a pattern for the unsigned float number"""
     return r"(?:(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][+-]?\d+)?)"
@@ -7,9 +10,12 @@ def float_number_pattern() -> str:
     ufn_format: str = unsigned_float_number_pattern()
     return rf"[+-]?{ufn_format}"
 
+def iterable_items_pattern(iterable: Iterable) -> str:
+    return rf"(?:{'|'.join([re.escape(str(k)) for k in iterable])})"
+
 def arithmetic_operations_pattern() -> str:
     """"Define a pattern that matches any of the following arithmetic operations: +, -, /, * and **"""
-    return r"(?:\+|\-|\*|/|\*\*)"
+    return iterable_items_pattern(['+','-','*','/','**'])
 
 def ltr_no_parentheses_expr(num_pattern: str, op_pattern: str) -> str:
     """Define a pattern for the valid arithmetic expression that does not contain parentheses

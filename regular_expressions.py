46b0d259c4d0b62f2aeb6095bb3d53228e7de0ba
srgsuh <srg.suhoverhov@gmail.com>
2025-11-25 12:35:14 +0200

    Implement patterns for arithmetic operations and arithmetic expression

--- regular_expressions.py
+++ regular_expressions.py
@@ -1,33 +1,18 @@
-def python_identifier_pattern() -> str:
-    return r'[a-z_A-Z]\w*'
-
-def pwd_pattern() -> str:
-    return r"(?=.*[A-Z])(?=.*[a-z])(?=.*[#$%])(?=.*[\d])[A-Za-z_#$%\d]{8,}"
-
-def ipv4_pattern() -> str:
-    """returns regexp as match pattern of IPv4 address
-       comprises of 4 octets separated by dot
-       each octet contains 1-3 symbols from 0 to 255
-    """
-    return  r"(([0-1]?\d{1,2}|25[0-5]|2[0-4][0-9])\.){3}([0-1]?\d{1,2}|25[0-5]|2[0-4][0-9])"
-
-def israel_mobile_pattern() -> str:
-    """returns regexp for mobile phone Israel number
-       +972- - Israel preffix (not mandatary)
-       Operator preffix 0 (only without +972-)
-       50,51, 52, 53, 54, 55, 56, 57,58, 59
-       optional dash
-       7 digits as follows
-       xxxxxxx
-       xxx-xx-xx
-       x-xx-xx-xx
-    """
-    
-    return r"(\+972-|0)5\d-?(\d{7}|(\d-?\d{2}-\d{2}-\d{2}))"
-
 def unsigned_float_number_pattern() -> str:
+    """Define a pattern for the unsigned float number"""
     return r"(?:(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][+-]?\d+)?)"
 
 def float_number_pattern() -> str:
+    """Define a pattern for the signed float number"""
     ufn_format: str = unsigned_float_number_pattern()
     return rf"[+-]?{ufn_format}"
+
+def arithmetic_operations_pattern() -> str:
+    """"Define a pattern that matches any of the following arithmetic operations: +, -, /, * and **"""
+    return r"(?:\+|\-|\*|/|\*\*)"
+
+def ltr_no_parentheses_expr(num_pattern: str, op_pattern: str) -> str:
+    """Define a pattern for the valid arithmetic expression that does not contain parentheses
+    with binary operations defined in the `op_pattern` and numbers in the format defined by the `num_pattern`
+    """
+    return rf"(?:{num_pattern}{op_pattern})*{num_pattern}"

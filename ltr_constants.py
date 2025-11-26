27db08a94ec961c76bae17bbbef8371d6a74c98e
srgsuh <srg.suhoverhov@gmail.com>
2025-11-26 11:53:11 +0200

    Implement get_operation function

--- ltr_constants.py
+++ ltr_constants.py
@@ -11,6 +11,12 @@ __operations: dict[str, Callable[[float, float], float]] = {
     '**': operator.pow
 }
 
+def get_operation(operation_sign: str) -> Callable[[float, float], float]:
+    operation_function = __operations.get(operation_sign)
+    if operation_function is None:
+        raise ValueError(f"Unsupported operation: {operation_sign}.")
+    return operation_function
+
 __NUMBER_PATTERN: str = reg_expr.unsigned_float_number_pattern()
 __OPERATION_PATTERN: str = reg_expr.iterable_items_pattern(__operations)
 __EXPR_PATTERN: str = reg_expr.ltr_expression(__NUMBER_PATTERN, __OPERATION_PATTERN)
@@ -19,7 +25,7 @@ __BLANK_PATTERN: str = r"\s+"
 
 
 ltr_pattern: Pattern = compile(__EXPR_PATTERN)
-simple_pattern: Pattern = compile(__INNER_EXPR_PATTERN)
+inner_pattern: Pattern = compile(__INNER_EXPR_PATTERN)
 operation_pattern: Pattern = compile(__OPERATION_PATTERN)
 number_pattern: Pattern = compile(__NUMBER_PATTERN)
 blank_pattern: Pattern = compile(__BLANK_PATTERN)

e194856943e8488bd5dd6e738b27b665290cc5b7
srgsuh <srg.suhoverhov@gmail.com>
2025-11-27 11:11:00 +0200

    Minor refactoring

--- ltr_constants.py
+++ ltr_constants.py
@@ -17,9 +17,10 @@ def get_operation(operation_sign: str) -> Callable[[float, float], float]:
         raise ValueError(f"Unsupported operation: {operation_sign}.")
     return operation_function
 
-__NUMBER_PATTERN: str = reg_expr.unsigned_float_number_pattern()
+__UNSIGNED_NUMBER_PATTERN: str = reg_expr.unsigned_float_number_pattern()
+__SIGNED_NUMBER_PATTERN: str = reg_expr.float_number_pattern()
 __OPERATION_PATTERN: str = reg_expr.iterable_items_pattern(__operations)
-__EXPR_PATTERN: str = reg_expr.ltr_expression(__NUMBER_PATTERN, __OPERATION_PATTERN)
+__EXPR_PATTERN: str = reg_expr.ltr_expression(__UNSIGNED_NUMBER_PATTERN, __OPERATION_PATTERN)
 __INNER_EXPR_PATTERN: str = reg_expr.inner_expression()
 __BLANK_PATTERN: str = r"\s+"
 
@@ -27,5 +28,5 @@ __BLANK_PATTERN: str = r"\s+"
 ltr_pattern: Pattern = compile(__EXPR_PATTERN)
 inner_pattern: Pattern = compile(__INNER_EXPR_PATTERN)
 operation_pattern: Pattern = compile(__OPERATION_PATTERN)
-number_pattern: Pattern = compile(__NUMBER_PATTERN)
+signed_num_pattern: Pattern = compile(__SIGNED_NUMBER_PATTERN)
 blank_pattern: Pattern = compile(__BLANK_PATTERN)

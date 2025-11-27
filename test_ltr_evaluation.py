3be5f1c239eefcda2d79e0ed29bfbc264828b909
srgsuh <srg.suhoverhov@gmail.com>
2025-11-27 11:09:45 +0200

    Add test cases with blanks inside the ltr and with negatives as partial computations results

--- test_ltr_evaluation.py
+++ test_ltr_evaluation.py
@@ -6,7 +6,7 @@ tol: int = 4
 class testLtrEvaluation(ut.TestCase):
     def test_eval_success(self) -> None:
         self.assertAlmostEqual(1e-4, eval("1e-4"), places=tol)
-        self.assertAlmostEqual(100, eval("((((1E+2))))"), places=tol)
+        self.assertAlmostEqual(100, eval("(100)"), places=tol)
         self.assertAlmostEqual(1, eval("5.000 +      5/1              /1e1"), places=tol)
         self.assertAlmostEqual(5, eval("2 + 3"), places=tol)
         self.assertAlmostEqual(16, eval("3**1.0 + (2*.1e+2/(40 - 20))+ (3*1E0 * 40.0*1e-1)"), places=tol)

7af369e43773901fb748321df51a1756a9be3b67
srgsuh <srg.suhoverhov@gmail.com>
2025-11-27 11:11:36 +0200

    Fix bugs with blanks inside the ltr and negatives emerging during the computation process

--- ltr_evaluation.py
+++ ltr_evaluation.py
@@ -1,7 +1,6 @@
 import regular_expressions as reg_expr
 from utils import is_parentheses_pairing
-from ltr_constants import blank_pattern, get_operation, ltr_pattern, inner_pattern, number_pattern, operation_pattern
-
+from ltr_constants import get_operation,blank_pattern,ltr_pattern,inner_pattern,signed_num_pattern
 
 def __is_valid_expression(expr: str) -> bool:
     return is_parentheses_pairing(expr) and (ltr_pattern.fullmatch(expr) is not None)
@@ -11,18 +10,20 @@ def __compute_one(op1: float, op2: float, operation_sign: str) -> float:
     return operation_function(op1, op2)
 
 def __eval_no_parentheses(expr: str) -> float:
-    operations: list[str] = number_pattern.split(expr)
-    operands: list[str] = number_pattern.findall(expr) # fix to work with sci-format (e.g. 1e-2)
-
+    print(f"expression = {expr}")
+    operations: list[str] = signed_num_pattern.split(expr)
+    operands: list[str] = signed_num_pattern.findall(expr) # fix to work with sci-format (e.g. 1e-2)
+    print(f"operation = {operations}, operand = {operands}")
     res = float(operands[0])
     for i in range(1, len(operands)):
-        res = __compute_one(res, float(operands[i]), operations[i])
+        operation_sign = operations[i] if operations[i] else '+'
+        res = __compute_one(res, float(operands[i]), operation_sign)
     return res
 
 def eval(expr: str) -> float:
-    expr = blank_pattern.sub("", expr)
     if not __is_valid_expression(expr):
         raise ValueError(f"Invalid expression: {expr}")
+    expr = blank_pattern.sub("", expr)
     while mo := inner_pattern.search(expr):
         inner = mo.group()[1:-1]
         value = __eval_no_parentheses(inner)
